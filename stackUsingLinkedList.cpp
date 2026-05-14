5da703b9d7d2b63bea79df0486773fba5690aceb
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-05-14 11:23:27 +0700

    menampilkan seluruh isi stack

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -48,5 +48,15 @@ class stack
             {
                 cout << "List is empty." << endl;
             }
+            else
+            {
+                Node *current = top;
+                while(current != NULL)
+                {
+                    cout << current -> data << " " << endl;
+                    current = current -> next;
+                }
+                cout << endl;
+            }
         }
 };
