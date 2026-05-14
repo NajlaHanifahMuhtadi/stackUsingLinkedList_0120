c3b1f16fdae0c0ca96ffe87a8239dc2d3fe1a9a5
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-05-14 11:21:52 +0700

    menambahkan validasi stack kosong pada peek

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -44,6 +44,9 @@ class stack
         }
         void peek()
         {
-            
+            if (top == NULL)
+            {
+                cout << "List is empty." << endl;
+            }
         }
 };
